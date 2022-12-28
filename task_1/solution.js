674687378b855700bb1c31c724dc5df4d8a5a635
AlexMarukka <marukka777@icloud.com>
2022-12-28 18:24:08 +0300

    my insert

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,8 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
     // Задание №1.1. Оповещение о добавлении в корзину
+
+    let message = productName;
 
     // создайте переменную message
     // присвойте переменной message значение переменной productName
@@ -14,6 +15,13 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    message = message + ' '; // проще все в одной итерации сделать message = message + ' за ' + productPrice + ' теперь в корзине!'
+    message = message + 'за';
+    message = message + ' ';
+    message = message + productPrice;
+    message = message + ' ';
+    message = message + 'теперь в корзине!';
+
     // Конец решения задания №1.1.
 
     return message;
@@ -23,10 +31,13 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+
+    newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -38,10 +49,13 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
+    let newSum = oldSum + difference;
+    //let newSumText = newSum + ' ₽';
+    let newSumText = `${newSum}  ₽`;
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
 
     // Конец решения задания №1.3.
 
